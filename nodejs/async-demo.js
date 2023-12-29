84d43d217da96670c75ad8cef5ccbba752448467
Aldora <aldora988@gmail.com>
2023-12-28 22:21:13 -0800

    Edit async task

--- nodejs/async-demo.js
+++ nodejs/async-demo.js
@@ -1,13 +1,24 @@
-async function asyncTask() {
-    console.log('2. Async task started');
-    // await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a 2-second async operation
-    new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a 2-second async operation
-    console.log('4. Async task finished');
+// await example
+async function asyncTask1() {
+    console.log('12. Async task started');
+    await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a 2-second async operation
+    console.log('14. Async task finished');
 }
 
-console.log('1. Before asyncTask call');
-asyncTask();
-console.log('3. After asyncTask call');
+console.log('11. Before asyncTask call');
+asyncTask1();
+console.log('13. After asyncTask call');
+
+// without await example
+async function asyncTask2() {
+    console.log('22. Async task started');
+    new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a 2-second async operation
+    console.log('23. Async task finished');
+}
+
+console.log('21. Before asyncTask call');
+asyncTask2();
+console.log('24. After asyncTask call');
 
 function resolveAfter2Seconds(x) {
     return new Promise((resolve) => {
