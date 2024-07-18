2afe27ec3c4edf57364e5d5403d585e3a05f980d
Aldora <aldora988@gmail.com>
2024-07-18 15:22:04 -0700

    Edit js-test

--- nodejs/js-test.js
+++ nodejs/js-test.js
@@ -1,4 +1,33 @@
-let obj = {"value": 1};
-let obj2 = {...obj, "value": 2, "value": 3};
+// let test = [{
+//   name: "narrow image",
+//   desc: "it will display a narrow feature image",
+//   link: "https://newspaper.kusa-projects.com/foreign-relations-diplomatic-talks-with-neighboring-countries/?post-layout=narrow&sidebar=false",
+// }, {
+//   name: "wide image",
+//   desc: "it will display a wide feature image",
+// }, {
+//   name: "image left",
+// }, {
+//   name: "with sidebar",
+//   desc: "it will display a sidebar of featured articles",
+//   link: "https://newspaper.kusa-projects.com/foreign-relations-diplomatic-talks-with-neighboring-countries/?post-layout=featured-image-left&sidebar=true"
+// }];
 
-console.log(obj2);
+// let test = "English,German,Spanish,French,Italian,Dutch"
+//   .split(",")
+//   .map(item => {return {name: item}})
+
+let test = [{
+  name: "home timeline",
+  link: "https://curie.brightthemes.com/"
+}, {
+  name: "home list",
+  link: "https://curie.brightthemes.com/home-2/"
+}, {
+  name: "home grid",
+  link: "https://curie.brightthemes.com/home-3/"
+}, {
+  name: "post wide image",
+  link: "https://curie.brightthemes.com/improved-pagination/"
+}]
+console.log(JSON.stringify(test));
